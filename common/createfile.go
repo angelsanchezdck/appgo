c7933634a161dc693b9d3e0a2ffb7241ac0cbd17
Angel Sanchez <25909832+angelsanchezdck@users.noreply.github.com>
2022-09-28 18:26:51 -0400

    update

--- common/createfile.go
+++ common/createfile.go
@@ -11,7 +11,9 @@ import (
 )
 
 func CreateFile(content *File, sess *session.Session) (*File, error) {
+	fmt.Printf("Error CreateFile 1")
 	output, err := uploadFile(content, sess)
+	fmt.Printf("Error CreateFile 2")
 	if err != nil {
 		fmt.Printf("error generating file: %v", err)
 		return nil, err
@@ -23,15 +25,21 @@ func CreateFile(content *File, sess *session.Session) (*File, error) {
 
 func uploadFile(content *File, sess *session.Session) (*s3manager.UploadOutput, error) {
 	uploader := s3manager.NewUploader(sess)
+	fmt.Printf("Error uploadFile 1")
 	input := &s3manager.UploadInput{
 		Bucket:      aws.String(content.Bucket),
 		Key:         aws.String(content.FileName),
 		Body:        bytes.NewReader([]byte(content.Content)),
 		ContentType: aws.String("text/plain"),
 	}
+	fmt.Printf("Error uploadFile 2")
 	output, err := uploader.UploadWithContext(context.Background(), input)
+	
+	fmt.Printf("Error uploadFile 3")
 	if err != nil {
 		fmt.Printf("Error uploading file: %v", err)
+		fmt.Printf(err)
+		fmt.Printf(output)
 		return nil, err
 	}
 
